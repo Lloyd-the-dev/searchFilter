70dfec18f0294f085123631f48069e23b8018055
Favour Ojo <ojoore35@gmail.com>
2023-09-16 20:47:51 +0100

    done

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+import JSONDATA from './MOCK_DATA.json'
+
+function App() {
+  const [searchItem, setSearchItem] = useState("")
+  
+
+  return (
+    <>
+     <div className='container'>
+      <h1>SEARCH FILTER</h1>
+        <input type="text" className='input' placeholder='search...' 
+        onChange={(event) => {
+          setSearchItem(event.target.value);
+        }}
+      />
+        {JSONDATA.filter((val) => {
+          if(searchItem == ""){
+            return val
+          }else if(val.first_name.toLowerCase().includes(searchItem.toLowerCase())){
+            return val
+          }
+        }).map(
+          (val, key) => {
+            return (
+              <div className='user' key={key}>
+                <p>{val.first_name}</p>
+              </div>
+            )
+          }
+        )}
+     </div>
+      
+     
+    </>
+  )
+}
+
+export default App
